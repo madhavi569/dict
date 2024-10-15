1d1b381b78397b8a3f68cdf3a87f0497a62a23d6
madhavi569 <madhavireddi27@gmail.com>
2024-10-15 10:53:10 +0530

    Create Items.py

---
+++ Items.py
@@ -0,0 +1,14 @@
+'''
+The program takes a dictionary and prints the sum of all the items in the dictionary.
+Problem Solution:
+1. Declare and initialize the n number of dictionary values from the user.
+2. Find the sum of all the values in the dictionary.
+3. Print the total sum.
+4. Exit.
+Sample Input:
+3
+100
+540
+239
+'''
+879
